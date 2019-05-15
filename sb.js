f94c160888d1113dcd98ba96884c9f1ed999c20a
szhu5759 <szhu5759@gmail.com>
2019-05-15 20:08:27 +0000

    Automatically backed up by Learn

--- sb.js
+++ sb.js
@@ -1,4 +1,6 @@
-myArray = [1,2,3,4,5]
-lengthOfArray = 1
-lengthOfArray -= myArray.length
-console.log(myArray.slice(lengthOfArray))
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+  return array
+}
+var x = [1,2,3]
+removeElementFromEndOfArray(x)
