be0689f28ed929176fac0c9652acf8e0b8b2d8ba
szhu5759 <szhu5759@gmail.com>
2019-05-15 20:02:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  const array
   array.slice(0, array.length - 1)
   return array
 }
