3cf871840954ee64921de718bc872df55f44e53b
szhu5759 <szhu5759@gmail.com>
2019-05-15 20:16:08 +0000

    Automatically backed up by Learn

--- sb.js
+++ sb.js
@@ -1,5 +1,5 @@
 var iceCreams = ["chocolate", "vanilla", "raspberry"]
- 
+
 iceCreams.slice(0, iceCreams.length - 1) // ["chocolate", "vanilla"]
- 
+
 iceCreams // ["chocolate", "vanilla", "raspberry"]
